95392377c34f29d4ef7709ea1e16f5a91c22182d
abilasha24 <selvanayagamabilasha@gmail.com>
2026-06-21 20:26:52 +0530

    fix: Static export for Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,28 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
-  },
+      { protocol: "https", hostname: "via.placeholder.com", port: "", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", port: "", pathname: "/**" }
+    ]
+  }
 };
-
 export default nextConfig;
